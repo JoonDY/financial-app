141fc8fe0572073c2bde9c41b6959687f6be53c8
Joon <daniel.joon.yi@gmail.com>
2022-01-22 21:02:54 -0800

    Error page

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Error = () => {
   return (
     <div>
-      <h2>Page Not Found</h2>
+      <h2 className="text-2xl font-semibold mx-auto text-center pt-20">
+        Page Not Found
+      </h2>
     </div>
   );
 };
